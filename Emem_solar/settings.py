e441bba8ac72f1efbb31e491ceddccd5d6df3637
futuregodwin <164335576+futuregodwin@users.noreply.github.com>
2026-04-09 15:41:48 +0100

    Update settings debug

--- Emem_solar/settings.py
+++ Emem_solar/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6ev(jo3hq!912f&3rwt(!@5xq5zb182#x7d0ymm0=0eepx+-9%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['69.10.53.253', 'ememenergy.com', 'www.ememenergy.com']
 
